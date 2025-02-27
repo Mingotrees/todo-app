58f1cdc54023ff7183aa40be3fbd4aaa553881e4
Mingotrees <jhanell.mingo@gmail.com>
2025-02-27 14:53:32 +0800

    javascript is cool

--- main/assets/script.js
+++ main/assets/script.js
@@ -1,8 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const input = document.getElementById('input-box');
-  
-    input.addEventListener('input', function () {
-      this.style.width = 'auto';
-      this.style.width = (this.scrollWidth + 2) + 'px';
-    });
-  });
+const addButton = document.getElementById("add");
+const inputBox = document.getElementById("input-box");
+
+function addItem() {
+    const toDoString = document.getElementById("input-box").value;
+    if (toDoString.trim() === "") return;
+
+    const parent = document.getElementById('todo-item-container');
+
+    const newItem = document.createElement('div');
+    newItem.classList.add('item-container', 'bg-amber-200');
+    newItem.id = 'todo-item';
+    newItem.innerHTML = `
+        <div class="flex justify-center items-center">
+            <input type="checkbox" name="task-complete" id="complete-btn" class="appearance-none w-4 h-4 bg-white checked:bg-green-400 text rounded-2xl">
+            <h2 class="ml-2">${toDoString}</h2>
+        </div>
+        <button id="del-btn" class="appearance-none w-9 h-8 cursor-pointer p-1 hover:bg-red-300 transition ease-in duration-300 rounded-2xl">
+            <img src="./assets/imgs/delete.svg">
+        </button>
+    `;
+
+    parent.appendChild(newItem);
+    document.getElementById("input-box").value = "";
+}
+
+function deleteItem(){
+    const parentRem = document.
+}
+
+addButton.addEventListener("click", addItem);
+
+inputBox.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addItem();
+    }
+});
