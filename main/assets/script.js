d7435a676f1df3402974b7c821b3f2bc62a82390
Jhanell Mingo <jhanell.mingo@gmail.com>
2025-02-28 23:24:33 +0800

    implemented delete and edit feature with the help of my friend

--- main/assets/script.js
+++ main/assets/script.js
@@ -1,6 +1,8 @@
 const addButton = document.getElementById("add");
 const inputBox = document.getElementById("input-box");
 
+// Methods
+
 function addItem() {
     const toDoString = document.getElementById("input-box").value;
     if (toDoString.trim() === "") return;
@@ -8,29 +10,68 @@ function addItem() {
     const parent = document.getElementById('todo-item-container');
 
     const newItem = document.createElement('div');
-    newItem.classList.add('item-container', 'bg-amber-200');
+    newItem.classList.add('item-container', 'flex', 'max-w-full', 'overflow-hidden', 'bg-amber-200');
     newItem.id = 'todo-item';
     newItem.innerHTML = `
-        <div class="flex justify-center items-center">
-            <input type="checkbox" name="task-complete" id="complete-btn" class="appearance-none w-4 h-4 bg-white checked:bg-green-400 text rounded-2xl">
-            <h2 class="ml-2">${toDoString}</h2>
-        </div>
-        <button id="del-btn" class="appearance-none w-9 h-8 cursor-pointer p-1 hover:bg-red-300 transition ease-in duration-300 rounded-2xl">
-            <img src="./assets/imgs/delete.svg">
-        </button>
+        <div class="flex items-center w-10/12">
+              <input
+                type="checkbox"
+                name="task-complete"
+                class="complete-btn appearance-none w-4 h-4 bg-white checked:bg-green-400 text rounded-2xl flex-shrink-0"
+              />
+              <h2 class="ml-2 todo-label">${toDoString}</h2>
+            </div>
+            <div class="flex justify-center items-center">
+              <button
+                class="edit-btn appearance-none cursor-pointer w-7 p-1 hover:bg-blue-300 transition ease-in duration-300 rounded-2xl"
+              >
+                <img src="./assets/imgs/edit.svg" />
+              </button>
+              <button
+                class="del-btn appearance-none cursor-pointer w-6 hover:bg-red-300 transition ease-in duration-300 rounded-2xl"
+              >
+                <img src="./assets/imgs/delete.svg" />
+              </button>
+            </div>
     `;
 
-    parent.appendChild(newItem);
+    parent.insertBefore(newItem, parent.firstChild);
     document.getElementById("input-box").value = "";
+
+    newItem.querySelector(".del-btn").addEventListener("click", removeItem);
+    newItem.querySelector(".edit-btn").addEventListener("click", editItem);
 }
 
-function deleteItem(){
-    const parentRem = document.
+function editItem() {
+    const item = this.closest('.item-container');
+    const temp = item.querySelector('.todo-label').textContent;
+    const label = item.querySelector('.todo-label');
+    const input = document.createElement('input');
+    input.classList.add('todo-new-input', 'ml-2', 'w-96', 'overflow-hidden');
+    input.value = temp;
+    label.replaceWith(input);
+    input.focus();
+
+    input.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            const newLabel = document.createElement('h2');
+            newLabel.classList.add('ml-2', 'todo-label', 'flex-wrap', 'overflow-hidden');
+            newLabel.textContent = input.value;
+            input.replaceWith(newLabel);
+        }
+    });
 }
+
+function removeItem() {
+    const item = this.closest('.item-container');
+    item.remove();
+}
+
+// Event Listeners
 
 addButton.addEventListener("click", addItem);
 
-inputBox.addEventListener("keydown", function(event) {
+inputBox.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         addItem();
     }
